1f5b01d96e1bd754bcd52a771b843b31aca01cd2
Tanner Hartwig <tanner.hartwig57@gmail.com>
2018-02-15 21:11:08 -0600

    Object Queries

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -2,22 +2,31 @@ import { createApolloServer } from 'meteor/apollo';
 import { makeExecutableSchema } from 'graphql-tools';
 import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql';
 
-const typeDefs = [`
-type Query {
-  hi: String
-}
-`];
-
 const testSchema = `
   type Query {
     hi: String
+    resolutions: [Resolution]
   }
 `;
+
+const typeDefs = [testSchema, ResolutionsSchema];
 
 const resolvers = {
   Query: {
     hi() {
       return 'Hello Level Up';
+    },
+    resolutions() {
+      return [
+        {
+          _id: '1234',
+          name: 'Get stuff done!',
+        },
+        {
+          _id: '4321',
+          name: 'Gain some weight!',
+        },
+      ];
     },
   },
 };
